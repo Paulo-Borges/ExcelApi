8ad6c10132e612df4c811e27f2e1405cc920f1be
Borges <borgespaulo72@yahoo.com.br>
2026-07-03 20:15:38 -0300

    feat: conexão com Banco SqlServer criada

--- ExcelApi/Data/AppDbContext.cs
+++ ExcelApi/Data/AppDbContext.cs
@@ -1,6 +1,9 @@
-﻿namespace ExcelApi.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExcelApi.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
     }
 }
